162f9d3da33bd4be35b8e38b2edf3082997d6d3f
Dmitry422 <yakovlev.kem@gmail.com>
2026-02-05 18:16:31 +0700

    clean

--- applications/main/subghz/scenes/subghz_scene_rpc.c
+++ applications/main/subghz/scenes/subghz_scene_rpc.c
@@ -2,7 +2,6 @@
 
 #include <lib/subghz/blocks/custom_btn.h>
 #include <lib/subghz/blocks/generic.h>
-//#include <lib/subghz/devices/devices.c>
 #include "applications/main/subghz/helpers/subghz_txrx_i.h"
 
 typedef enum {
